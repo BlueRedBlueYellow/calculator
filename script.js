be1b5af3fa191f84cffb7c34754e6e237c7a4f2c
BlueRedBlueYellow <74393018+BlueRedBlueYellow@users.noreply.github.com>
2023-07-13 19:51:02 -0500

    Make fuctions for each math operation

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ let firstNumber;
 let secondNumber;
 let currentDisplay;
 
+function add(int1, int2) {
+    return int1 + int2;
+}
+
+function subtract(int1, int2) {
+    return int1 - int2;
+}
+
+function multiply(int1, int2) {
+    return int1 * int2;
+}
+
+function divide(int1, int2) {
+    return int1 / int2;
+}
+
+function operate(operation, int1, int2) {
+    return operation(int1, int2)
+}
